c81f0e5beaa0990d388690de1dafb195193acf14
tqhuy04 <tranquanghuy0413@gmail.com>
2025-07-09 21:02:08 +0700

    first commit

---
+++ src/main/java/com/example/Hoc_Java_mua_xuan/HocJavaMuaXuanApplication.java
@@ -0,0 +1,13 @@
+package com.example.Hoc_Java_mua_xuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HocJavaMuaXuanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HocJavaMuaXuanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Hoc_Java_mua_xuan/HocJavaMuaXuanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Hoc_Java_mua_xuan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HocJavaMuaXuanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
